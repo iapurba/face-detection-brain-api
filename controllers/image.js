c1ab55459af3c354c2225981bbc84a32d6f10c1e
Apurba Panja <apurbapanja1@gmail.com>
2020-11-24 00:01:55 +0530

    Add clarifai api call functionality

--- controllers/image.js
+++ controllers/image.js
@@ -1,3 +1,18 @@
+const Clarifai =  require('clarifai');
+
+const app = new Clarifai.App({
+	apiKey: 'ae46f3ee7b9546f4bb2cfa9f2f67ffd2'
+});
+
+const handleApiCall = (req, res) => {
+	app.models
+		.predict (Clarifai.FACE_DETECT_MODEL, req.body.input)
+		.then(data => {
+			res.json(data);
+		})
+		.catch(err => res.status(400).json('unable to work with api'))
+}
+
 const handleImage = (req, res, db) => {
   const { id } = req.body;
   db('users')
@@ -11,5 +26,6 @@ const handleImage = (req, res, db) => {
 }
 
 module.exports = {
-	handleImage: handleImage
+	handleImage,
+	handleApiCall
 }
--- server.js
+++ server.js
@@ -25,15 +25,11 @@ app.use(bodyParser.json());
 app.use(cors());
 
 app.get('/', (req, res) => { res.send('It is working.') });
-
 app.post('/signin', (req, res) => { signin.handleSignin(req, res, db, bcrypt) });
-
 app.post('/signup', (req, res) => { signup.handleSignup(req, res, db, bcrypt) });
-
 app.get('/profile/:id', (req, res) => { profile.handleProfileGet(req, res, db) });
-
 app.put('/image', (req, res) => { image.handleImage(req, res, db) });
-
+app.post('/imageurl', (req, res) => { image.handleApiCall(req, res) });
 
 app.listen(3000, () => {
   console.log('app is running on port 3000');
