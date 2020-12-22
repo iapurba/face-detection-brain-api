481b6e7cddecb04e99b9b575085c92138d30b288
Apurba Panja <apurbapanja1@gmail.com>
2020-12-22 20:36:07 +0530

    Updated

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const bcrypt = require('bcrypt-nodejs');
 const cors = require('cors');
+const morgan = require('morgan');
 const knex = require('knex')
 
 const signin = require('./controllers/signin');
@@ -12,17 +13,13 @@ const image = require('./controllers/image');
 
 const db = knex({
   client: 'pg',
-  connection: {
-    connectionString: process.env.DATABASE_URL,
-    ssl: {
-      rejectUnauthorized: false
-    } 
-  }
+  connection: process.env.POSTGRES_URI
 });
 
 const app = express();
 app.use(bodyParser.json());
 app.use(cors());
+app.use(morgan('combined'));
 
 app.get('/', (req, res) => { res.send('It is working.') });
 app.post('/signin', (req, res) => { signin.handleSignin(req, res, db, bcrypt) });
@@ -31,8 +28,9 @@ app.get('/profile/:id', (req, res) => { profile.handleProfileGet(req, res, db) }
 app.put('/image', (req, res) => { image.handleImage(req, res, db) });
 app.post('/imageurl', (req, res) => { image.handleApiCall(req, res) });
 
-const port = process.env.PORT;
 
-app.listen(port, () => {
-  console.log(`Server is listening to port ${port}`);
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+  console.log(`Server is listening to port ${PORT}`);
 })
